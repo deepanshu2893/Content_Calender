15e20b91a510672f8aa50432beee1d54e07c6414
Deepanshu Panwar <deepanshu2893@gmail.com>
2023-09-26 00:07:18 +0530

    Content Calender

--- content-calender/src/main/java/deepanshu/panwar/contentcalender/controller/ContentController.java
+++ content-calender/src/main/java/deepanshu/panwar/contentcalender/controller/ContentController.java
@@ -1,10 +1,27 @@
 package deepanshu.panwar.contentcalender.controller;
 
 
+import deepanshu.panwar.contentcalender.model.Content;
+import deepanshu.panwar.contentcalender.repository.ContentCollectionRepository;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/content")
 public class ContentController {
+
+    private ContentCollectionRepository repository;
+
+    public ContentController (ContentCollectionRepository repository){
+        this.repository = repository;
+    }
+
+    @GetMapping("")
+    public List<Content> findAll(){
+        return repository.findAll();
+
+    }
 }
--- content-calender/src/main/java/deepanshu/panwar/contentcalender/repository/ContentCollectionRepository.java
+++ content-calender/src/main/java/deepanshu/panwar/contentcalender/repository/ContentCollectionRepository.java
@@ -2,8 +2,12 @@ package deepanshu.panwar.contentcalender.repository;
 
 
 import deepanshu.panwar.contentcalender.model.Content;
+import deepanshu.panwar.contentcalender.model.Status;
+import deepanshu.panwar.contentcalender.model.Type;
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -22,5 +26,19 @@ public class ContentCollectionRepository {
     public Optional <Content> findById(Integer id){
         return content.stream().filter(c -> c.id().equals(id)).findFirst();
 
+    }
+
+    @PostConstruct
+    private void init(){
+        Content c = new Content(1,
+                "My First Blog Post",
+        "My first blog post",
+                Status.IDEA,
+                Type.ARTICLE,
+                LocalDateTime.now(),
+        null,
+        "");
+
+        content.add(c);
     }
 }
